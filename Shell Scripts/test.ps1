8b380308fbbaf88f8287bb9341141f44bef88a7a
JayAlvinCVallescas <202080209@psu.palawan.edu.ph>
2024-02-13 19:25:26 +0800

    add ascii art

--- Shell Scripts/test.ps1
+++ Shell Scripts/test.ps1
@@ -1,9 +1,21 @@
+$asciiArt = @"
+ ____                  _      _                     
+/ ___|___  _   _ _ __ | |_ __| | _____      ___ __  
+| |   / _ \| | | | '_ \| __/ _` |/ _ \ \ /\ / / '_ \ 
+| |__| (_) | |_| | | | | || (_| | (_) \ V  V /| | | |
+\____\___/ \__,_|_| |_|\__\__,_|\___/ \_/\_/ |_| |_|
+                                                                                                        
+"@
+
+Write-Host $asciiArt `n` `n` `n`
+
+                                                     
 for ($i = 5; $i -gt 0; $i--) {
-    Write-Host -NoNewline "Restarting in $i seconds... "
+    Write-Host -NoNewline "Restarting in $i seconds..."
     Start-Sleep -Seconds 1
     Write-Host -NoNewline "`r" # Move the cursor to the beginning of the line
     Write-Host -NoNewline 
 }
 
 Write-Host ""
-Write-Host "Time's up!!!"
+Write-Host "Time's up!!!" `n`
