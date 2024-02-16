e8e118578cefe2b0322d51444f5de8015fadcb42
JayAlvinCVallescas <202080209@psu.palawan.edu.ph>
2024-02-16 15:56:39 +0800

    modify rot 13

--- Python_Scripts/ROT13/rot.py
+++ Python_Scripts/ROT13/rot.py
@@ -3,16 +3,27 @@
 
 import argparse
 import codecs
+from colorama import Fore, Style, init
 
 
 def rot13(input_string):
     return codecs.encode(input_string, 'rot13')
 
 def main():
+    init()
     parser = argparse.ArgumentParser(
-        description="A ROT13 script made with python\n\n"
-                     "Example: python rot.py \"hello\"\n\n"
-                     "Result: uryyb ",
+    description= Fore.GREEN + """
+===================================== 
+|               ROT 13              |
+=====================================
+                               
+>>> Made by ZownHori
+>>> https://github.com/JayAlvinCVallescas/Scripts/tree/main/Python_Scripts/ROT13
+-------------------------------------------------------------------------------------------            
+"""
+    "Description: A ROT13 script made with python\n\n"
+    "Example: python rot.py \"hello\"\n"
+    "Result: uryyb ",
         formatter_class=argparse.RawDescriptionHelpFormatter
     )   
     parser.add_argument("input_string", nargs="?", help="String to be encoded with ROT13")
