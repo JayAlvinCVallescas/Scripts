89813b98379175d356a811a6c1db0e1da68634ba
JayAlvinCVallescas <202080209@psu.palawan.edu.ph>
2024-02-16 14:10:22 +0800

    add file

---
+++ Python_Scripts/HelpTest/help_test.py
@@ -0,0 +1,23 @@
+#! usr/bin/python
+
+from colorama import Fore, Back, Style, init
+import argparse
+
+ascii_art = Fore.GREEN + """
+ _____         _                 
+|_   _|       | |                
+  | | ___  ___| |_   _ __  _   _ 
+  | |/ _ \/ __| __| | '_ \| | | |
+  | |  __/\__ \ |_ _| |_) | |_| |
+  \_/\___||___/\__(_) .__/ \__, |
+                    | |     __/ |
+                    |_|    |___/ 
+"""
+init()
+
+print(ascii_art)
+
+description = """ 
+Test sample 
+
+"""
