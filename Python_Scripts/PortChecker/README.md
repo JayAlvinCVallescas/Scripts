2e1195f3508728d863bffbec53554c6228c24538
JayAlvinCVallescas <202080209@psu.palawan.edu.ph>
2024-02-16 15:36:29 +0800

    modify port checker

---
+++ Python_Scripts/PortChecker/README.md
@@ -0,0 +1,16 @@
+# Port Checker python script :white_flag:
+
+<br>
+Description:
+
+- Port Checker is a Python script for scanning target IP addresses to identify open or closed ports commonly used by network services.
+
+---
+
+<br>
+Usage:
+
+- python pc.py
+- Then enter the IP
+
+---
--- Python_Scripts/PortChecker/pc.py
+++ Python_Scripts/PortChecker/pc.py
@@ -1,9 +1,30 @@
 import socket
+from colorama import Fore, init, Style
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.settimeout(2)                                      #2 Second Timeout
-result = sock.connect_ex(('127.0.0.1',80))
-if result == 0:
-  print('port OPEN')
-else:
-  print('port CLOSED, connect_ex returned: '+str(result))
+sock.settimeout(2)
+
+def main(): 
+    init()
+    ascii_art = Fore.GREEN + """
+=====================================
+|            PORT CHECKER           |
+=====================================
+>>> Made by ZownHori
+>>> https://github.com/JayAlvinCVallescas/Scripts/tree/main/Python_Scripts/PortChecker
+----------------------------------------------------------------------------------------------------"""
+    common_ports = [20, 21, 22, 23, 25, 53, 80, 110, 137, 139, 143, 443, 445, 1433, 1434, 3306, 3389, 8080, 8443]  # Add more ports as needed
+    print(ascii_art)
+    target = input("IP: ")
+    print(Fore.GREEN + f"\nTARGET IP: {target}\nPORT      STATUS")
+    
+    for port in common_ports:
+        result = sock.connect_ex((target, port))
+        if result == 0:
+            status = "OPEN"
+        else:
+            status = "CLOSE"
+        print(f"{port:<10} {status}")
+
+if __name__ == "__main__":
+    main()
